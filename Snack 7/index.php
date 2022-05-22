8c1e3b97d1bc70c0c20c5be65173924fedc33674
Ilthepriest <robertodelprete1993@libero.it>
2022-05-22 19:06:31 +0200

    soluzione 2

--- Snack 7/index.php
+++ Snack 7/index.php
@@ -6,7 +6,7 @@ $alunniClasse = [
     [
         "nome" => "Marcello",
         "cognome" => "Del Vecchio",
-        "voti" => [7, 5, 8, 4, 6, 9 ]
+        "voti" => [7, 5, 8, 4, 6, 9 ]   
     ],
     [
         "nome" => "Francesco",
@@ -35,14 +35,33 @@ $alunniClasse = [
     ],
 ];
 
-  for ($i=0; $i < count($alunniClasse[$i]["voti"]) ; $i++) { 
-    $voti = $alunniClasse[$i]["voti"];
-    $votiToString = implode("-" ,$voti);
-    $alunno = $alunniClasse[$i];
-    $somma = (array_sum($alunniClasse[$i]["voti"]));
-    $media = $somma / count($alunniClasse[$i]["voti"]);
-    echo $alunno["nome"]. " " .$alunno["cognome"]." " ."<br>";
-    echo "VOTI". " ". $votiToString. " <br>";
-    echo "MEDIA". " ". $media. "<br>";
+$somma = 0;
+//SOLUZIONE 1
+
+//CICLO SINGOLO STUDENTE E POI CICLO I SUOI VOTI UNA VOLTA FINITO PASSO AL SECONDO E COSI VIA...
+  for ($i=0; $i < count($alunniClasse) ; $i++) { 
+    $alunno = $alunniClasse[$i];  // singolo studente
+    var_dump($alunno["nome"]);
+    var_dump($alunno["cognome"]);
+    $somma = 0; //azzero la somma prima di partire col prossimo studente
+    for ($j=0; $j <count($alunno["voti"]) ; $j++) { 
+        $votoPerStudente = $alunno["voti"][$j];  //ciclo ogni singolo voto nel array "voti di uno studente"
+        $somma = $somma + $votoPerStudente;
+    }
+    var_dump($somma);
+}
+
+// SOLUZIONE 2
+
+//CICLO OGNI STUDENTE TRAMITE INDICE ED ENTRO NEI SUOI VOTI
+    for ($i=0; $i < count($alunniClasse[$i]["voti"]); $i++) { 
+        $voti = $alunniClasse[$i]["voti"];
+        $votiToString = implode("-" ,$voti); //implodo i voti per mostrarli distanziati da un trattino
+        $alunno = $alunniClasse[$i];
+        $somma = (array_sum($alunniClasse[$i]["voti"]));  //tramite la funzione sommo i voti dello studente corrispondente all'indice in cui mi trovo
+        $media = $somma / count($voti);
+        echo $alunno["nome"]. " " .$alunno["cognome"]." " ."<br>";
+        echo "VOTI". " ". $votiToString. " <br>";
+        echo "MEDIA". " ". $media. "<br>";
     }
 ?>
